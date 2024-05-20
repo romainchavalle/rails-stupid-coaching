901a7b4a90282c430dc7e25c4d95c905d40876a9
romain chavalle <romain.chavalle@gmail.com>
2024-05-20 14:54:28 +0200

    exo2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def Controller
+
+  end
+  def ask
+
+  end
+
+  def answer
+    if params[:ask] ==  "I am going to work"
+      @answer = "Great!"
+    elsif params[:ask].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+
+  end
+end
